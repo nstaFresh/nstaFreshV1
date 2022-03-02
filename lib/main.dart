d2dde2f02ebd2d645c4054e691b9173100b416ac
ibrahimeatspie <104ibrahimshah@gmail.com>
2022-03-02 13:11:29 -0600

    Stripe payment processing now working

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
 import 'main.dart';
 import 'route_generator.dart';
 import 'home.dart';
 import 'book_cleaning.dart';
 import 'payment.dart';
 
-void main() => runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Stripe.merchantIdentifier = 'I DRINK WATER';
+  Stripe.publishableKey = 'pk_test_pPq9ppt7qkZ1cNMBebnGtOlS';
+  await Stripe.instance.applySettings();
+
+  runApp(const MyApp());
+}
 
 //Color c = new Color.fromRGBO(0, 0, 0, 1.0);
 
